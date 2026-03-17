555b5c290747a2cf784a87525773db7e7402e225
Gobun-code <hamulskimaciej@gmail.com>
2026-03-18 00:19:04 +0100

    clean up unused elements and started adding local storage

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-
-
-
-
-
-
-
-
-
-function pokazalert() {
-    alert("działa");
-}
-
-
-
 function aktualizacjaLicznika() {
     const maxMisji = 10;
     const licznik = document.getElementById('licznik');
     const liczbaMisji = document.getElementById('tasklist').children.length;
     const procent = liczbaMisji / maxMisji;
-    licznik.textContent = "liczba misji: " + liczbaMisji;
+    licznik.textContent = "task count: " + liczbaMisji;
     const g = 255 * procent;
     const r = 255 * (1 - procent);
 licznik.style.color = `rgb(${r}, ${g}, 0)`;
 licznik.style.fontSize = `${35 + (liczbaMisji * 1.25)}px`;
 }
 
+
+
+function zapiszListe() {
+    const tasklist = document.querySelectorAll('#tasklist li');
+    const tasks = [];
+    
+    tasklist.forEach(li => {
+        tasks.push({
+            tekst: li.textContent,
+            zrobione: li.classList.contains('zrobione')
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    console.log('Lista zadań została zapisana:', tasks);
+}
 
 function addTask() {
     const input =
@@ -50,6 +50,7 @@ function addTask() {
         }
         else {
         li.classList.toggle('zrobione')
+        zapiszListe()
         }
     }
 
@@ -58,12 +59,11 @@ function addTask() {
 
     document.getElementById("tasklist").appendChild(li)
 
+    zapiszListe()
+
     aktualizacjaLicznika()
 
     input.value = "";    
-
-
-    
 }
 
 
@@ -72,7 +72,6 @@ document.getElementById("taskInput").addEventListener("keydown", function(event)
     if (event.key === "Enter") {
         addTask()
     }
-    
 })
 
 
